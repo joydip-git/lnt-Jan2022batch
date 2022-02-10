4a916a0f8e59841da39a4edc001593669d95c5f1
Joydip Mondal <joydip.mondal@gmail.com>
2022-02-10 10:12:46 +0530

    code and notes updated

---
+++ codes/day-2/SampleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10; //4 bytes
+            int b = 20;//010101100
+            int result = a + b;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
