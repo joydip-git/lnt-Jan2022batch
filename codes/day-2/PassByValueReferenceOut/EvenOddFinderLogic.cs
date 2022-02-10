a7d4672741811f1096494e03db46fa65ddd7c83b
Joydip Mondal <joydip.mondal@gmail.com>
2022-02-10 17:50:47 +0530

    end of day-5 update

---
+++ codes/day-2/PassByValueReferenceOut/EvenOddFinderLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PassByValueReferenceOut
+{
+    class EvenOddFinderLogic
+    {
+        public static bool IsEvenOrOdd(int data)
+        {
+            if (data % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ codes/day-2/PassByValueReferenceOut/UserInterface.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PassByValueReferenceOut
+{
+    class UserInterface
+    {
+        static int GetNumberFromUser()
+        {
+            Console.Write("enter a number: ");
+            string num = Console.ReadLine();
+            int number = int.Parse(num);
+            return number;
+        }
+        
+        static void Main()
+        {
+            //accept values from user
+            int value = GetNumberFromUser();
+            //call other functions, pass these accepted values and get the return value
+            bool status = EvenOddFinderLogic.IsEvenOrOdd(value);
+            //print the return value(s) in the console
+            if(status)
+                Console.WriteLine($"{value} is even");
+            else
+                Console.WriteLine($"{value} is odd");
+        }
+    }
+}
