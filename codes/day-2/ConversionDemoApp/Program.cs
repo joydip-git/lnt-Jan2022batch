9240d993845f28ea85adcd27fcf2a1c0fd29ba3e
Joydip Mondal <joydip.mondal@gmail.com>
2022-02-10 16:02:19 +0530

    conversion code update

---
+++ codes/day-2/ConversionDemoApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConversionDemoApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            //implicit conversion (Value type --> value type)
+            //int x = 10;
+            //long l = x;
+            //Console.WriteLine(l);
+
+            //explicit conversion (value type --> value type)
+            //long l = 1234567898967543;
+            //int x = checked((int)l);
+            //Console.WriteLine(x);
+
+
+            //implicit conversion (value type --> reference type)
+            //int a = 12;
+            //string numStr = a.ToString();
+            //Console.WriteLine(numStr);
+
+            //explicit conversion  (reference type -->  value type)
+            string str = "12";
+            //int num = int.Parse(str);
+            int num = Convert.ToInt32(str);
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ codes/day-2/EvenOddFinderApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace EvenOddFinderApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Program.IsEven();
+            //WriteLine: prints something in the console and then adds a newline character (\n)
+            //WriteLine: prints something in the console and does not add a newline character (\n)
+
+            Console.Write("enter a number: ");
+
+            //ReadLine: accepts value/input (complete stream) from user in the console
+            string strNumber = Console.ReadLine(); //12 --> "12"--> 12
+            //convert the string format value into actual number format
+
+            //Parse method ONLY converts string format to target, is possible
+            int number = int.Parse(strNumber);
+
+            //Any method (for ex. ToInt32()) of Convert class can convert ANY TYPE of data to target, if possible
+            //int number = Convert.ToInt32(strNumber);
+           
+            //int number = 5;
+            bool status = IsEvenOrOdd(number);
+            if (status)
+                Console.WriteLine($"{number} is even");
+            else
+                Console.WriteLine($"{number} is odd");
+        }
+
+        static bool IsEvenOrOdd(int value)
+        {
+            //if (value % 2 == 0)
+            //    Console.WriteLine($"{value} is even");
+            //else
+            //    Console.WriteLine($"{value} is odd");
+
+            //business logic
+            if (value % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
