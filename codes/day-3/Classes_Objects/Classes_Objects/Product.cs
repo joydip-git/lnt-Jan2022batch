4a22365be307ad3d6a27b8400f08ca8deb57249c
Joydip Mondal <joydip.mondal@gmail.com>
2022-02-11 15:51:51 +0530

    class object update

---
+++ codes/day-3/Classes_Objects/Classes_Objects/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Objects
+{
+    class Product
+    {
+        //class data members or fields
+        public int productId; //=0
+        public string productName; //=NULL
+        public decimal price; //=0
+        public string description; //=NULL
+    }
+}
---
+++ codes/day-3/Classes_Objects/Classes_Objects/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Classes_Objects
+{
+    class Program
+    {
+        static Product AcceptProductValues()
+        {
+            Console.Write("enter product name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("enter product id: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.Write("enter product price: ");
+            decimal price = decimal.Parse(Console.ReadLine());
+
+            Console.Write("enter product description: ");
+            string description = Console.ReadLine();
+
+            //productObject is a local variable here, which will store the initial address of the object's memory blok in the heap
+            //since the variable is storing the address of an object of type Product class, then the data type should be Product (class name)
+            Product productObject = null;
+
+            //now by 'new Product()' you creating an object in the heap with 4 memory blocks (productId, productName, price, description) with default values (0,NULL,0,NULL)
+            //by ''
+            productObject = new Product();
+
+            productObject.productId = id;
+            productObject.productName = name;
+            productObject.price = price;
+            productObject.description = description;
+
+            return productObject;
+        }
+        static void Main()
+        {
+           Product productObjRef = AcceptProductValues();
+            Console.WriteLine($"Name:{productObjRef.productName}, Id:{productObjRef.productId}, Price:{productObjRef.price}, Description:{productObjRef.description}");
+        }
+    }
+}
