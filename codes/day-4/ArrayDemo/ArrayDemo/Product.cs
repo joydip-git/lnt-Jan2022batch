ccda0a55d5d94886b8c81829a0137109309b4cf2
Joydip Mondal <joydip.mondal@gmail.com>
2022-02-14 17:16:52 +0530

    code and image of array uploaded

---
+++ codes/day-4/ArrayDemo/ArrayDemo/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayDemo
+{
+    class Product
+    {
+        readonly int productId;
+        string productName;
+        decimal price;
+
+        public int ProductId => this.productId;
+        public string ProductName { set => this.productName = value; get => this.productName; }
+        public decimal Price { set => this.price = value; get => this.price; }
+
+        public Product() { }
+        public Product(int pid) { this.productId = pid; }
+        public Product(int pid, string name, decimal price)
+        {
+            this.productId = pid;
+            this.productName = name;
+            this.price = price;
+        }
+    }
+}
---
+++ codes/day-4/ArrayDemo/ArrayDemo/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace ArrayDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            //employee emp = new employee();
+            //1,2,3,4,5,6
+            //array is a collection of similar type of values
+            //you create an object of Array class
+            //Array arr = new Array();<--do not create the object in this way
+            //create the array in the following way
+
+            //<data-type-of-values>[] <arr-var-name> = new <data-type-of-values>[<size>];
+
+            //Array itself is a reference type
+            //array of value types (stores values directly)
+            int[] arr = new int[5];
+
+            //arr[0] = 10;
+            //arr[1] = 20;
+            //arr[2] = 30;
+            //arr[3] = 40;
+            //arr[4] = 50;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"enter value at arr[{i}]: ");
+                int number = int.Parse(Console.ReadLine());
+                arr[i] = number;
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"value at arr[{i}]:{arr[i]}");
+            }
+
+            Product dellProduct = new Product(1, "dell xps", 100000);
+            Product onePlusProduct = new Product(2, "One Plus 9", 67000);
+            Product hpLaptopProduct = new Product(3, "HP Probook", 78000);
+
+            //array of refernce types (stores references/addresses)
+            Product[] products = new Product[3];
+
+            products[0] = dellProduct;
+            products[1] = onePlusProduct;
+            products[2] = hpLaptopProduct;
+
+            Console.WriteLine("\n\n");
+            for (int i = 0; i < products.Length; i++)
+            {
+                Product productRef = products[i];
+                Console.WriteLine($"Name:{productRef.ProductName}");
+            }
+
+            Console.WriteLine("\n\n");
+
+            //array of value types
+            int[,] twoDimArr = new int[2, 3];
+
+            for (int rowNo = 0; rowNo < 2; rowNo++)
+            {
+                for (int indexNo = 0; indexNo < 3; indexNo++)
+                {
+                    Console.Write($"enter value at twoDimArr[{rowNo},{indexNo}]: ");
+                    int number = int.Parse(Console.ReadLine());
+                    twoDimArr[rowNo, indexNo] = number;
+                }
+            }
+            for (int rowNo = 0; rowNo < 2; rowNo++)
+            {
+                for (int indexNo = 0; indexNo < 3; indexNo++)
+                {
+                    Console.WriteLine($"value at twoDimArr[{rowNo},{indexNo}]: { twoDimArr[rowNo, indexNo]}");                   
+                }
+            }
+
+            /*
+            for (int i = 0; i < 5; i++)
+            {
+                SayHello();
+            }
+            for (int i = 5; i > 0; i--)
+            {
+                SayHello();
+            }
+            */
+        }
+        static void SayHello()
+        {
+            Console.WriteLine("hello...");
+        }
+    }
+}
