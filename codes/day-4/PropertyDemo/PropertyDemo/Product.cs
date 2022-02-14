0bff451cee01dac5cecadd8f73f2f586fa95975e
Joydip Mondal <joydip.mondal@gmail.com>
2022-02-14 15:15:57 +0530

    property and read-only update

---
+++ codes/day-4/PropertyDemo/PropertyDemo/Product.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PropertyDemo
+{
+    class Product
+    {
+        //a readonly data member can be assigned ONLY through CONSTRUCTOR
+        private readonly int productId;
+        private string productName;
+        private decimal price;
+        private string description;
+
+        public Product() { }
+
+        public Product(int id)
+        {
+            this.productId = id;
+        }
+
+        public Product(int id, string name, decimal price, string description)
+        {
+            this.productId = id;
+            this.productName = name;
+            this.price = price;
+            this.description = description;
+        }
+
+        /*
+        public int ProductId
+        {
+            get { return this.productId; }
+        }
+
+        public int ProductId
+        {
+            //the productId is readonly, ONLY constructor can be used to set the value
+            //set => this.productId = value;
+            get => this.productId;
+        }
+        */
+
+        public int ProductId => this.productId;
+
+        public string ProductName
+        {
+            get => this.ProductName;
+            set => this.productName = value;
+        }
+
+        public decimal Price
+        {
+            get => this.price;
+            set => this.price = value;
+        }
+
+        public string Description
+        {
+            get => this.description;
+            set => this.description = value;
+        }
+
+        public string GetInfo()
+        {
+            return $"Name:{productName}, Price:{price}, Id: {productId}, description:{description}";
+        }
+    }
+}
---
+++ codes/day-4/PropertyDemo/PropertyDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PropertyDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Product dellProduct = new Product(1, "dell xps 13", 100000, "new laptop from dell");
+            //Console.WriteLine(dellProduct.GetInfo());
+            //Console.WriteLine(dellProduct.GetName());
+            //dellProduct.SetName("dell xps 15");
+            //Console.WriteLine(dellProduct.GetName());
+
+            //calling the property an assigning (like passing value to the method) a value: calling set accessor
+            dellProduct.ProductName = "dell xps 15";
+            //calling the property, but not assigning any value: calling the get accessor
+            Console.WriteLine(dellProduct.ProductName);
+
+            Product onePlusNineProduct = new Product(2);
+
+            onePlusNineProduct.ProductName = "One Plus 9";
+            onePlusNineProduct.Price = 67000;
+            onePlusNineProduct.Description = "New mobile from One Plus";
+
+            Console.WriteLine(onePlusNineProduct.GetInfo());
+        }
+    }
+}
