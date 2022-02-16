f63dc1c7120b9dbff9991df49cadd709d756c54f
Joydip Mondal <joydip.mondal@gmail.com>
2022-02-16 17:37:43 +0530

    interface update

---
+++ codes/day-5/InterfaceDemo/InterfaceDemo/IBankOperation.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceDemo
+{
+    //interface: pure abstract class
+    interface IBankOperation
+    {
+        decimal CreditIntoAccont(decimal amount);
+        decimal DebitFromAccount(decimal amount);
+        string CreateAccount();
+        string Name { set; get; }
+    }
+    interface IAdditionalServices
+    {
+        string OpenDematAccount();
+        string UseOnlineFacility();
+    }
+    //implement the interface
+    //abstract class
+    abstract class BankAccount : IBankOperation, IAdditionalServices
+    {
+        string accountHolderName;
+        decimal accountBalance;
+        public string Name
+        {
+            get => accountHolderName;
+            set => accountHolderName = value;
+        }
+        public decimal AccountBalance { get => accountBalance; set => accountBalance = value; }
+
+        public decimal CreditIntoAccont(decimal amount)
+        {
+            accountBalance = accountBalance + amount;
+            return accountBalance;
+        }
+
+        public decimal DebitFromAccount(decimal amount)
+        {
+            accountBalance = accountBalance - amount;
+            return accountBalance;
+        }
+
+        //since in class, nothing is by default public and abstract, that's why you have to use public and abstract keyword to write the member that you do not want to implement
+        public abstract string CreateAccount();
+
+        public string OpenDematAccount()
+        {
+            return "demat account opened";
+        }
+
+        public string UseOnlineFacility()
+        {
+            return "providing online money transaction facility";
+        }
+    }
+
+    //concrete class
+    class SavingsAccount : BankAccount
+    {
+        public override string CreateAccount()
+        {
+            return "Bank account created...";
+        }
+    }
+
+    class CurrentAccount : BankAccount
+    {
+        public override string CreateAccount()
+        {
+            return "Current Bank account created...";
+        }
+    }
+}
---
+++ codes/day-5/InterfaceDemo/InterfaceDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace InterfaceDemo
+{
+    interface IA
+    {
+        void M1();
+    }
+    interface IB
+    {
+        void M2();
+    }
+    class MyCls : IA, IB
+    {
+        public void M1() { }
+        public void M2() { }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            SavingsAccount joydipAcc = new SavingsAccount();
+            joydipAcc.Name = "Joydip";
+            joydipAcc.AccountBalance = 1000;
+            Console.WriteLine(joydipAcc.CreateAccount());
+            decimal currentBalance = joydipAcc.CreditIntoAccont(1000);
+            Console.WriteLine($"current balance of {joydipAcc.Name} is {joydipAcc.AccountBalance}");
+
+            //only members of that interface can be called
+            IBankOperation account = new CurrentAccount();
+            account.CreateAccount();
+            account.CreditIntoAccont(1000);
+            account.DebitFromAccount(100);
+            account.Name = "Anil";
+        }
+    }
+}
