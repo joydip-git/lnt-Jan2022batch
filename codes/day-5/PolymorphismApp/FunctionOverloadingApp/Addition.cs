28c34fe979f7d9a586f6cb16248cabe7272bb288
Joydip Mondal <joydip.mondal@gmail.com>
2022-02-16 13:07:50 +0530

    polymorphism - compile time polymorphism uploaded (code and notes)

---
+++ codes/day-5/PolymorphismApp/FunctionOverloadingApp/Addition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionOverloadingApp
+{
+    class Addition
+    {
+        public int Add(int firstNumber, int secondNumber)
+        {
+            return (firstNumber + secondNumber);
+        }
+        public int Add(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            return (firstNumber + secondNumber + thirdNumber);
+        }
+        public long Add(int firstNumber, int secondNumber, long thirdNumber)
+        {
+            return (firstNumber + secondNumber + thirdNumber);
+        }
+        public short Add(int firstNumber, long secondNumber, int thirdNumber)
+        {
+            return (short)(firstNumber + secondNumber + thirdNumber);
+        }
+        public double Add(double firstNumber, double secondNumber, double thirdNumber)
+        {
+            return (firstNumber + secondNumber + thirdNumber);
+        }
+    }
+}
---
+++ codes/day-5/PolymorphismApp/FunctionOverloadingApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FunctionOverloadingApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Addition addition = new Addition();
+
+            //caller line
+            Console.WriteLine(addition.Add(10, 20));
+
+            //caller line
+            Console.WriteLine(addition.Add(12.34, 13.45, 56.7));
+        }
+    }
+}
