90b11b30abd580d7ffcfef7257566177c3639869
Joydip Mondal <joydip.mondal@gmail.com>
2022-02-17 11:58:59 +0530

    collections and generics update

---
+++ codes/day-6/CollectionsAndGenerics/CollectionsDemo/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CollectionsDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            /*
+            int[] arr = new int[] { 1, 2, 3 };
+
+            //Length property of the array gives you the size of the array
+            int[] temp = new int[arr.Length];
+            arr.CopyTo(temp, 0);
+           
+            arr = new int[4];
+            Array.Copy(temp, arr, temp.Length);
+            arr[arr.Length-1]= 4;
+
+            temp = null;
+            */
+
+            //int[] arr = new int[2];
+            //arr[0] = 10;
+
+            //Array, built-in class, is an abstract class
+            //You can not create object of that class
+            //Array arr = new Array();
+
+            //object or Object
+
+            //Value type --> Reference Type (Boxing)
+            //implicit conversion
+            //value type variable (numVal)
+            int numVal = 12;
+            //rference type variable (intVal)
+            object intVal = numVal;
+
+            //that refernce type to same value type (unboxing)
+            //explicit conversion
+            int x = (int)intVal;
+
+            double fractionalVal = 12.34;
+            Object doubleVal = fractionalVal;
+            Object strVal = "LnT";
+            Object charVal = 'a';
+
+            //collection class: ArrayList
+            //advantages:
+            //no size restriction
+            //dynamic, keep on adding or removing value, the collection will be resized
+
+            //disadvantages:
+            //all the values of different type are boxed into object type, which is time consuimg
+            //when you iterate the values from the collection, the values will be unboxed again
+            //since all the elements are of Object type now, they are hard to sort, because down-the-line they are all of different data types
+            ArrayList al = new ArrayList();
+            int a = 1;
+            object aVal = a;
+            al.Add(aVal);
+            //al.Add(1);
+            al.Add('a');
+            al.Add(12.34);
+            al.Add("LnT");
+            //Count property gives the number of elements currently in the collection
+            for (int i = 0; i < al.Count; i++)
+            {
+                Console.WriteLine(al[i]);
+            }
+            Console.WriteLine("\n\n");
+            al.Remove(12.34);
+            foreach (object item in al)
+            {
+                Console.WriteLine(item);
+            }
+
+            //int[] arr = new int[] { 4, 1, 2, 3 };
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    for (int j = i + 1; j < arr.Length; j++)
+            //    {
+            //        if (arr[i] > arr[j])
+            //        {
+            //            int temp = arr[i];
+            //            arr[i] = arr[j];
+            //            arr[j] = temp;
+            //        }
+            //    }
+            //}
+            Console.WriteLine("\n\n");
+
+            //generic collection class
+            List<int> numberList = new List<int>();
+            numberList.Add(1);
+            numberList.Add(12);
+            numberList.Add(5);
+            numberList.Add(5);
+            numberList.Add('a');
+
+            numberList.Sort();
+            for (int i = 0; i < numberList.Count; i++)
+            {
+                Console.WriteLine(numberList[i]);
+            }
+            Console.WriteLine("\n");
+            numberList.Remove('a');
+            foreach (int item in numberList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\n\n");
+            List<string> names = new List<string>();
+            names.Add("Akash"); //0
+            names.Add("Swapnil");//1
+            //the insertion index value <= Count property value [0 --> 2]
+            names.Insert(2, "Deepthi");
+            //names.InsertRange(1,new List<string> {"", "",""});
+            names.Sort();
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("\n");
+            names.RemoveAt(1);
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            //names.RemoveRange(0, 2);            
+        }
+    }
+}
---
+++ codes/day-6/CollectionsAndGenerics/OtherGenericClasses/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OtherGenericClasses
+{
+    class Program
+    {
+        static void Main()
+        {
+            //this collection allows only unique values to be added
+            HashSet<int> set = new HashSet<int>();
+            set.Add(1);
+            set.Add(12);
+            set.Add(1);
+            set.Add(13);
+
+            foreach (int item in set)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\n");
+            set.Remove(12);
+            foreach (int item in set)
+            {
+                Console.WriteLine(item);
+            }
+
+            //keys should be unique, whereas values could be duplicate
+            //Dictionary is similar to HashTable class in System.Collections
+            //HashTable hash = new HashTable();
+            //hash.Add(1,"");
+            //hash.Add('a',12.34);
+            /*
+             * foreach (DictionaryEntry item in hash)
+            {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+            }
+             */
+
+            Dictionary<int, string> keyValuePairs = new Dictionary<int, string>();
+            keyValuePairs.Add(1, "Deepthi"); //KeyValuePair<int,str>
+            keyValuePairs.Add(0, "Giri");
+            keyValuePairs.Add(2, "Mansi");
+            //this line will throw exception, since you are using the duplicate key
+            //Add method is used to just add a key value pair, NOT TO UPDATE value of existing key
+            //keyValuePairs.Add(2, "Abhishek");
+            Console.WriteLine("\n\n");
+            foreach (KeyValuePair<int, string> item in keyValuePairs)
+            {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+            }
+
+            keyValuePairs.Remove(1);
+            Console.WriteLine("\n");
+            foreach (KeyValuePair<int, string> item in keyValuePairs)
+            {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+            }
+
+            //updating the existing value of the given key
+            keyValuePairs[2] = "Abhishek";
+
+            //since the key does not exist, the key as well as the value will be added in the collection
+            keyValuePairs[3] = "Adanki";
+            Console.WriteLine("\n");
+            foreach (KeyValuePair<int, string> item in keyValuePairs)
+            {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+            }
+
+            //SortedList<> is similar to SortedList class in System.Collections
+            //SortedList sl = new SortedList();
+            //make sure the keys are of similar type (required for sorting the keys)
+            //sl.Add(1,"");
+            //sl.Add(0,12.34);
+            /*
+             * foreach (DictionaryEntry item in sl)
+            {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+            }
+             */
+
+            //difference with Dictionary is: Dictionary<> does not sort the keys, whereas SortedList<> sorts the keys
+            SortedList<int, string> sortedList = new SortedList<int, string>();
+            sortedList.Add(1, "Deepthi"); //KeyValuePair<int,str>
+            sortedList.Add(0, "Giri");
+            sortedList.Add(2, "Mansi");
+            //this line will throw exception, since you are using the duplicate key
+            //Add method is used to just add a key value pair, NOT TO UPDATE value of existing key
+            //sortedList.Add(2, "Abhishek");
+            Console.WriteLine("\n\n");
+            foreach (KeyValuePair<int, string> item in sortedList)
+            {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+            }
+
+            sortedList.Remove(1);
+            Console.WriteLine("\n");
+            foreach (KeyValuePair<int, string> item in sortedList)
+            {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+            }
+
+            //updating the existing value of the given key
+            sortedList[2] = "Abhishek";
+
+            //since the key does not exist, the key as well as the value will be added in the collection
+            sortedList[3] = "Adanki";
+            Console.WriteLine("\n");
+            foreach (KeyValuePair<int, string> item in sortedList)
+            {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+            }
+        }
+    }
+}
