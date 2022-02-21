d459d756fba3d0980314b0e8d7b1c5a306db8ba9
Joydip Mondal <joydip.mondal@gmail.com>
2022-02-21 13:34:32 +0530

    DOM, JS and app updated

---
+++ codes/web/day-1/js-introduction/source/index.js
@@ -0,0 +1,16 @@
+//document object represents the HTML document to which this JS file is linked
+var btnClickObj = document.getElementById('btnClick')
+//btnClickObj.addEventListener('click', clickMe);
+btnClickObj.onclick = clickMe
+
+function clickMe() {
+    btnClickObj.textContent = "Wow...You clicked Me";
+    window.alert('button clicked')
+}
+function clickMeFirst() {
+    window.alert('button first clicked')
+    // var allScriptTags = document.getElementsByName('script')
+    // var scriptTag = allScriptTags[0]
+    // window.alert(scriptTag)
+}
+
