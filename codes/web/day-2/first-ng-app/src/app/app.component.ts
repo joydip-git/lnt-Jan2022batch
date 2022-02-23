37277cc103a8c0603a7c8e9671a319bc4d062ac5
Joydip Mondal <joydip.mondal@gmail.com>
2022-02-23 18:21:14 +0530

    angular update

---
+++ codes/web/day-2/first-ng-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+/**
+ * built-in decorator function @Component()
+ */
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+}
