6b0b67192f45d5bb81781bbb8cad0b7b64aee2e4
joydip mondal <joydip.mondal@gmail.com>
2022-02-24 16:37:24 +0530

    data binding upload

--- codes/web/day-3/ng-databinding-app/src/app/app.component.ts
+++ codes/web/day-3/ng-databinding-app/src/app/app.component.ts
@@ -12,8 +12,29 @@ import { Component } from '@angular/core'
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
+//model-view-viewmodel (MVVM)
+//MVC (model-virew-controller)
+//message, personName etc. are model data
+//the design of the component is view
+//the component class is the controller, controlling how the data flows between the design and the code (to and from)
 export class AppComponent {
+    //everything in the class are by default 'public' in TS
+    //component data or model or component property
+    message: string = 'Soumya';
+    pWidth: number = 200; //=> "200"
+    pId: string = "namePara"
+    personName: string = "NA";
+
     constructor() {
         console.log('App component object created')
     }
+    callMe(val: string) {
+        window.alert(`welcome ${val}`)
+        //$"{}"
+    }
+
+    // show(val: string) {
+    //     this.personName = val
+    //     console.log(this.personName)
+    // }
 }
--- codes/web/day-3/ng-databinding-app/src/app/app.module.ts
+++ codes/web/day-3/ng-databinding-app/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   /** register the names of components, pipes and directives in the array of 'declarations' property */
   declarations: [AppComponent],
   /** register the names of other modules in the array of 'imports' property */
   /**BrowserModule is the one which brings all assets required to run the Angular app in the browser. Register it in the root module only */
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   /** register the names of services in the array of 'providers' property */
   providers: [],
   bootstrap: [AppComponent]
