113ccba195bb3de54cda5e43e5a52bac9bd81729
Joydip Mondal <joydip.mondal@gmail.com>
2022-02-25 13:11:57 +0530

    Create products.module.ts

---
+++ codes/web/day-4/product-management-app/src/app/products/products.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+
+@NgModule({
+    declarations: [],
+    providers: [],
+    imports: [],
+    bootstrap: [],
+    exports: []
+})
+
+export class ProductsModule {
+
+}
