49ce797aec93a7b2a5470f44f231c1a4e8d9f4f3
Joydip Mondal <joydip.mondal@gmail.com>
2022-03-02 16:49:21 +0530

    assignment created

---
+++ codes/devops/WordCounterLibrary/WordCounterLibrary/WordCounter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordCounterLibrary
+{
+    //in this application you can create additional private functions, callable from the given function
+    public class WordCounter
+    {
+        /*
+         * this method accepts a string and returns a SortedList<string,int> containing every word in the string in alphabetical order and how many times the words have appeard in that string
+         * sample input=>
+         * DotNet Core is a technology, which is interoperable and also DotNet Core is platform neutral
+         * 
+         * sample output=> 
+         * a: 1
+         * Core: 2
+         * DotNet: 2
+         * is: 3
+         * .......
+         */
+        public SortedList<string,int> CountWordFrequency(string input)
+        {
+            return null;
+        }
+       
+    }
+}
