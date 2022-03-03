96ccafce0766b637b750371623ba8374dec6d98b
Joydip <joydip.mondal@gmail.com>
2022-03-03 18:06:57 +0530

    App updated

--- codes/dotnet/product-management-system/ProductManagementSystem/ProductManagementSystem.DataAccessLayer/Implementation/ProductDaoImpl.cs
+++ codes/dotnet/product-management-system/ProductManagementSystem/ProductManagementSystem.DataAccessLayer/Implementation/ProductDaoImpl.cs
@@ -2,8 +2,8 @@
 using ProductManagementSystem.DataAccessLayer.Contract;
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Data;
+using System.Data.SqlClient;
 
 namespace ProductManagementSystem.DataAccessLayer.Implementation
 {
@@ -15,7 +15,37 @@ namespace ProductManagementSystem.DataAccessLayer.Implementation
 
         public bool DeleteRecord(int id)
         {
-            throw new NotImplementedException();
+            bool status = false;
+            try
+            {
+                connection = new SqlConnection("server=joydip-pc;database=lntdb;user id=sa;password=sqlserver2017");
+                command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandText = "DeleteProduct";
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@pid", id)
+                    .SqlDbType = SqlDbType.Int;
+
+                connection.Open();
+                int result = command.ExecuteNonQuery();
+                if (result > 0)
+                    status = true;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (connection != null && connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+            return status;
         }
 
         public List<Product> GetAllRecords()
@@ -51,7 +81,7 @@ namespace ProductManagementSystem.DataAccessLayer.Implementation
                     }
                 }
             }
-            catch(SqlException ex)
+            catch (SqlException ex)
             {
                 throw ex;
             }
@@ -72,7 +102,56 @@ namespace ProductManagementSystem.DataAccessLayer.Implementation
 
         public Product GetARecordById(int id)
         {
-            throw new NotImplementedException();
+            Product product = null;
+            try
+            {
+                connection = new SqlConnection("server=joydip-pc;database=lntdb;user id=sa;password=sqlserver2017");
+                command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandText = "GetProductById";
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@pid", id).SqlDbType = SqlDbType.Int;
+
+                //select count(productname) from products
+                // command.ExecuteScalar(); => used to executeselect query which returns single value
+                connection.Open();
+                reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {                   
+                    while (reader.Read())
+                    {
+                        product = new Product
+                        {
+                            ProductId = (int)reader["productid"],
+                            ProductName = (string)reader["productname"],
+                            Price = (decimal)reader["price"],
+                            Description = (string)reader["description"],
+                            ReleaseDate = (DateTime)reader["releasedate"],
+                            StarRating = (decimal)reader["rating"],
+                            ImagePath = (string)reader["imagepath"],
+                            CategoryId = (int)reader["categoryid"]
+                        };
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+
+                if (connection != null && connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+            return product;
         }
 
         public List<Product> GetPoductsByName(string name)
@@ -87,12 +166,113 @@ namespace ProductManagementSystem.DataAccessLayer.Implementation
 
         public bool InsertRecord(Product newData)
         {
-            throw new NotImplementedException();
+            bool status = false;
+            try
+            {
+                connection = new SqlConnection("server=joydip-pc;database=lntdb;user id=sa;password=sqlserver2017");
+                command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandText = "InsertProduct";
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@pname", newData.ProductName)
+                    .SqlDbType = SqlDbType.VarChar;
+                command.Parameters.AddWithValue("@price", newData.Price)
+                    .SqlDbType = SqlDbType.Decimal;
+                command.Parameters.AddWithValue("@cid", newData.CategoryId)
+                    .SqlDbType = SqlDbType.Int;
+                command.Parameters.AddWithValue("@rating", newData.StarRating)
+                    .SqlDbType = SqlDbType.Decimal;
+                command.Parameters.AddWithValue("@imgpath", newData.ImagePath)
+                    .SqlDbType = SqlDbType.VarChar;
+                command.Parameters.AddWithValue("@releasedate", newData.ReleaseDate)
+                    .SqlDbType = SqlDbType.Date;
+                command.Parameters.AddWithValue("@desc", newData.Description)
+                    .SqlDbType = SqlDbType.VarChar;
+
+                /*
+                SqlParameter pname = new SqlParameter();
+                pname.ParameterName = "@pname";
+                pname.Value = newData.ProductName;
+                pname.SqlDbType = SqlDbType.VarChar;
+
+                SqlParameter price = new SqlParameter();
+                price.ParameterName = "@price";
+                price.Value = newData.Price;
+                price.SqlDbType = SqlDbType.Decimal;
+
+                command.Parameters.Add(pname);
+                command.Parameters.Add(price);
+                */
+
+                connection.Open();
+                int result = command.ExecuteNonQuery();
+                if (result > 0)
+                    status = true;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (connection != null && connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+            return status;
         }
 
         public bool UpdateRecord(Product updatedData)
         {
-            throw new NotImplementedException();
+            bool status = false;
+            try
+            {
+                connection = new SqlConnection("server=joydip-pc;database=lntdb;user id=sa;password=sqlserver2017");
+                command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandText = "UpdateProduct";
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@pid", updatedData.ProductId)
+                    .SqlDbType = SqlDbType.Int;
+                command.Parameters.AddWithValue("@pname", updatedData.ProductName)
+                    .SqlDbType = SqlDbType.VarChar;
+                command.Parameters.AddWithValue("@price", updatedData.Price)
+                    .SqlDbType = SqlDbType.Decimal;
+                command.Parameters.AddWithValue("@cid", updatedData.CategoryId)
+                    .SqlDbType = SqlDbType.Int;
+                command.Parameters.AddWithValue("@rating", updatedData.StarRating)
+                    .SqlDbType = SqlDbType.Decimal;
+                command.Parameters.AddWithValue("@imgpath", updatedData.ImagePath)
+                    .SqlDbType = SqlDbType.VarChar;
+                command.Parameters.AddWithValue("@releasedate", updatedData.ReleaseDate)
+                    .SqlDbType = SqlDbType.Date;
+                command.Parameters.AddWithValue("@desc", updatedData.Description)
+                    .SqlDbType = SqlDbType.VarChar;
+
+                connection.Open();
+                int result = command.ExecuteNonQuery();
+                if (result > 0)
+                    status = true;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (connection != null && connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+            return status;
         }
     }
 }
--- codes/dotnet/product-management-system/ProductManagementSystem/ProductManagementSystem.DataAccessLayerTests/ProductDaoImplTest.cs
+++ codes/dotnet/product-management-system/ProductManagementSystem/ProductManagementSystem.DataAccessLayerTests/ProductDaoImplTest.cs
@@ -1,6 +1,6 @@
 using NUnit.Framework;
-using ProductManagementSystem.DataAccessLayer.Implementation;
 using ProductManagementSystem.BusinessEntities;
+using ProductManagementSystem.DataAccessLayer.Implementation;
 using System.Collections.Generic;
 
 namespace ProductManagementSystem.DataAccessLayerTests
@@ -14,9 +14,15 @@ namespace ProductManagementSystem.DataAccessLayerTests
             productDao = new ProductDaoImpl();
         }
 
+        [TearDown]
+        public void Clear()
+        {
+            productDao = null;
+        }
+
         [Test]
         public void GetAllRecordsTest()
-        {            
+        {
             //variable declared with var keyword is known as implicitly typed local variable (actualRecords)
             var actualRecords = productDao.GetAllRecords();
             var expectedRecords = new List<Product>
@@ -24,6 +30,56 @@ namespace ProductManagementSystem.DataAccessLayerTests
                 new Product{ ProductId=100, ProductName="dell xps", CategoryId=1, Price=10000.00M, Description="new product from dell", ImagePath="NA", ReleaseDate=new System.DateTime(2022,1,1), StarRating=4.5M }
             };
             CollectionAssert.AreEqual(expectedRecords, actualRecords);
+            //Product p1= expectedRecords[0];
+            //Product p2= actualRecords[0];
+            //p1.Equals(p2)
+            //Assert.AreEqual(expectedRecords[0].ProductId, actualRecords[0].ProductId);
+        }
+
+        [Test]
+        public void InsertRecordTest()
+        {
+            //object initializer
+            Product sample = new Product
+            {
+                ProductName = "hp probook",
+                Price = 67000.00M,
+                CategoryId = 1,
+                Description = "new product from hp",
+                ImagePath = "NA",
+                StarRating = 4,
+                ReleaseDate = new System.DateTime(2022, 1, 12)
+            };
+            var actualStatus = productDao.InsertRecord(sample);
+            Assert.AreEqual(true, actualStatus);
+        }
+
+        [Test]
+        public void UpdateRecordTest()
+        {
+            //object initializer
+            Product sample = new Product
+            {
+                ProductId = 102,
+                ProductName = "one plus 9",
+                Price = 70000.00M,
+                CategoryId = 2,
+                Description = "new mobile from one plus",
+                ImagePath = "NA",
+                StarRating = 4.5M,
+                ReleaseDate = new System.DateTime(2022, 1, 15)
+            };
+            var actualStatus = productDao.UpdateRecord(sample);
+            Assert.AreEqual(true, actualStatus);
+        }
+
+        [Test]
+        public void GetARecordByIdTest()
+        {
+            //variable declared with var keyword is known as implicitly typed local variable (actualRecords)
+            var actualRecord = productDao.GetARecordById(100);
+            var expectedRecord = new Product { ProductId = 100, ProductName = "dell xps", CategoryId = 1, Price = 10000.00M, Description = "new product from dell", ImagePath = "NA", ReleaseDate = new System.DateTime(2022, 1, 1), StarRating = 4.5M };
+            Assert.AreEqual(expectedRecord, actualRecord);
             //Product p1= expectedRecords[0];
             //Product p2= actualRecords[0];
             //p1.Equals(p2)
