000044b9b5361f5b2af2cd435f1c29e09d075f60
Joydip <joydip.mondal@gmail.com>
2022-03-07 17:52:40 +0530

    files deleted

--- codes/aspnet-apps/revision/RevisionApp/LinqImplementation/Program.cs
+++ codes/aspnet-apps/revision/RevisionApp/LinqImplementation/Program.cs
@@ -4,8 +4,7 @@ using System.Collections.Generic;
 namespace LinqImplementation
 {
     class Program
-    {       
-
+    {   
         static void Main()
         {
             List<Student> allStudents = StudentRepository.GetStudents();
