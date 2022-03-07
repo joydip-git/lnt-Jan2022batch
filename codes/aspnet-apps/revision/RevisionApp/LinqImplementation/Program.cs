9fb70dd2379ffa09c0693d082ac448f09022d969
Joydip <joydip.mondal@gmail.com>
2022-03-07 17:42:07 +0530

    LINQ implementation created and readied for practice

---
+++ codes/aspnet-apps/revision/RevisionApp/LinqImplementation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqImplementation
+{
+    class Program
+    {       
+
+        static void Main()
+        {
+            List<Student> allStudents = StudentRepository.GetStudents();
+        }
+    }
+}
---
+++ codes/aspnet-apps/revision/RevisionApp/LinqImplementation/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqImplementation
+{
+    public class Student
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+        public int ID { get; set; }
+        public List<int> Scores;
+    }
+}
