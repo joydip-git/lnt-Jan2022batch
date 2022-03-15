08cfcda783b50cec1f90929ed9879d38b50adbfd
Joydip <joydip.mondal@gmail.com>
2022-03-15 12:06:17 +0530

    registered CORS in ConfihureServices Method

--- projects/server/BankingSystemApp/BankingSystemApp.BankingAPIService/Startup.cs
+++ projects/server/BankingSystemApp/BankingSystemApp.BankingAPIService/Startup.cs
@@ -24,6 +24,7 @@ namespace BankingSystemApp.BankingAPIService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers();
         }
 
