6dc249ae4f585518398b4f32e78fc6d8447a1b48
Joydip <joydip.mondal@gmail.com>
2022-03-15 12:17:58 +0530

    created method in login controller, created data access layer interface and class

---
+++ projects/server/BankingSystemApp/BankingSystemApp.BankingAPIService/DataAccessLayer/ILoginDao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankingSystemApp.BankingAPIService.DataAccessLayer
+{
+    public interface ILoginDao
+    {
+        string Authenticate(string uname, string pwd);
+    }
+}
---
+++ projects/server/BankingSystemApp/BankingSystemApp.BankingAPIService/DataAccessLayer/LoginDao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankingSystemApp.BankingAPIService.DataAccessLayer
+{
+    public class LoginDao : ILoginDao
+    {
+        public string Authenticate(string uname, string pwd)
+        {
+            return null;
+        }
+    }
+}
